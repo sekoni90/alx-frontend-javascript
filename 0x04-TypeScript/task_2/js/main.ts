9cb696495764ef3645f27588c7fb28427da0c2c5
sekoni90 <sekonioyetunji@gmail.com>
2025-12-12 15:34:48 +0000

    FINAL: Complete task_2 with isDirector and executeWork functions

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -71,8 +71,8 @@ console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
 
-console.log(executeWork(createEmployee(200))); // Getting to work
-console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+console.log(executeWork(createEmployee(200))); // Expected: Getting to work
+console.log(executeWork(createEmployee(1000))); // Expected: Getting to director tasks
 
 console.log(teachClass('Math'));
 console.log(teachClass('History'));
