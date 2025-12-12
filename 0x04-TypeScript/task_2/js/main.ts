068db22eacd376ca41bd2a0f9529e06f54fb6a52
sekoni90 <sekonioyetunji@gmail.com>
2025-12-12 15:14:08 +0000

    Add teachClass function to task_2 and create complete task_3 with CRUD operations

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,5 +57,18 @@ function executeWork(employee: Director | Teacher): string {
   }
 }
 
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
