14a3e065a8b2bdfdeab4b2a7fa4680812d1ecbb9
sekoni90 <sekonioyetunji@gmail.com>
2025-12-12 15:26:57 +0000

    Fix teachClass parameter format: remove space after colon

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,7 +59,7 @@ function executeWork(employee: Director | Teacher): string {
 
 type Subjects = 'Math' | 'History';
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
   } else {
